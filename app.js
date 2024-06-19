886d0710f00363a0812c0dbcad89f1c3d9ed0246
krayush1109 <hellotoayush@gmail.com>
2024-06-19 11:54:55 +0530

    EXPRESS App Generator - Boiler Plate

--- app.js
+++ app.js
@@ -1,29 +1,41 @@
-// dotenv config
-require('dotenv').config({ path: './.env' })
-// console.log(process.env.PORT)
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-// express config
-const express = require('express')
-const app = express()
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
-const path = require("path");
+var app = express();
 
-// db connection
-require('./models/db').connectDB();
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-app.set("view engine", "ejs")
-app.set("views", path.join(__dirname, "views"))
-
+app.use(logger('dev'));
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-const userRoutes = require("./routes/user.routes")
-// app.use("/", userRoutes);
-app.use("/user", userRoutes);
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
-// server
-app.listen(process.env.PORT, () => {
-    console.log("Server is runnig on PORT: ")
-    // console.log("Server is runnig on PORT: ", process.env.PORT)
-})
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
 
-console.log("Social Media - APP");
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
